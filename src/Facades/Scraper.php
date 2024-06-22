75b9304cc4b9e0ff6487cc7d8a10686cc9809e94
Greg Priday <greg@siteorigin.com>
2024-06-22 14:35:03 +0200

    Add IDE hints

--- src/Facades/Scraper.php
+++ src/Facades/Scraper.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \GregPriday\Scraper\Scraper
+ *
+ * @method static \Psr\Http\Message\ResponseInterface get(string $url, array $options = [])
+ * @method static \GuzzleHttp\Client getClient()
  */
 class Scraper extends Facade
 {
